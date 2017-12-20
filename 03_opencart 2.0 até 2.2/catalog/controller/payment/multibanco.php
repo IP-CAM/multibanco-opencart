4fcd3f3b77b7164e370587fee7563f4a15dac54c
Manuel Rocha <manuelrocha@manuelrocha.biz>
2017-12-20 16:19:20 +0000

    Update multibanco.php
    fix template loading on ajax invocation for opencart 2.0 to 2.2

--- 03_opencart 2.0 até 2.2/catalog/controller/payment/multibanco.php
+++ 03_opencart 2.0 até 2.2/catalog/controller/payment/multibanco.php
@@ -7,11 +7,7 @@ class ControllerPaymentMultibanco extends Controller {
 
 		$data['continue'] = $this->url->link('checkout/success');
 
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/multibanco.tpl')) {
-			return $this->load->view($this->config->get('config_template') . '/template/payment/multibanco.tpl', $data);
-		} else {
-			return $this->load->view('default/template/payment/multibanco.tpl', $data);
-		}
+		return $this->load->view('payment/multibanco', $data);
 	}
 
 	public function version(){
